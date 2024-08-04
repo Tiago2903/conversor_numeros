cfce3582297b2fb4063c384d3ead96220b84e345
Tiago2903 <xaloaxolot@gmail.com>
2024-08-04 18:51:50 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+class Convesor {
+	constructor(num) {
+		this.num = num;
+	}
+
+	valorEntrada(num) {
+		if (typeof (num) === 'string') {
+			return this.romanoParaDecimal(num);
+		} else {
+			return this.decimalParaRomano(num);
+
+		}
+	}
+
+	romanoParaDecimal(num) {
+		const input = num.split('');
+		const valoresRomanos = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000
+		};
+
+		let total = 0;
+
+		input.forEach((el, id) => {
+			const valAtual = valoresRomanos[el]
+			const proxVal = valoresRomanos[input[id + 1]]
+
+			// Se o proximo valor não existir, ou for maior que o valor atual, significa que posso somar o valor atual ao total,
+			// Caso o proximo valor exista e seja menor, significa que é uma subtração.
+			if (proxVal && valAtual < proxVal) {
+				total -= valAtual;
+			} else {
+				total += valAtual;
+			}
+		});
+
+		return total;
+	}
+
+	decimalParaRomano(num) {
+		let mapeamentoRomanos = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+		let algarismosRomanos = '';
+
+		Object.keys(mapeamentoRomanos).forEach((el) => {
+			if (num >= mapeamentoRomanos[el]) {
+				num -= mapeamentoRomanos[el]
+				algarismosRomanos += el
+			}
+		})
+
+		return algarismosRomanos
+	}
+}
+
+function numeroEscolhido(val) {
+	const conversor = new Convesor();
+	const resultado = conversor.valorEntrada(val);
+	console.log(resultado);
+}
+
+numeroEscolhido('MCMXLIX');
+numeroEscolhido(1949);
